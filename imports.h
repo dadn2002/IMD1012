ec2d3621cac3a705fa071176da866e0436d82149
dadn2002 <nmdavialves@gmail.com>
2024-06-05 03:26:53 -0300

    first try

---
+++ imports.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
+
+#define okay(msg, ...) printf("[+] " msg "\n", ##__VA_ARGS__)
+#define warn(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
+#define info(msg, ...) printf("[i] " msg "\n", ##__VA_ARGS__)
+#define debug(msg, ...) printf("[%] " msg "\n", ##__VA_ARGS__)
+
+// pointer to an array of 3 pointers to const char.
+using BoardType = const char*(*)[3]; 
+
+const char* playerSymbol = "X";
+const char* aiSymbol     = "O";
